5d1341712d7c6d4aee79c4210050a95722496758
Taras Onyshko <tasik7231@gmail.com>
2019-08-19 21:24:50 +0300

    first commit

---
+++ SECOND/Task_2.1/Task_2.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1
+{
+    public abstract class Figure
+    {
+        public abstract void Draw();
+
+    }
+
+    class Rectangle : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I`m Rectangle!");
+        }
+
+    }
+
+    class Square : Figure
+    {
+        
+        public override void Draw()
+        {
+            Console.WriteLine("I`m Square!");
+        }
+
+    }
+    
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square obj1 = new Square();
+            Rectangle obj2 = new Rectangle();
+            obj1.Draw();
+            obj2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SECOND/Task_2.1/Task_2.2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._2
+{
+
+
+    public abstract class Figure
+    {
+        double X { get; }
+        double Y { get; }
+        public abstract void Draw();
+
+        public Figure(double a, double b)
+        {
+            this.X = a;
+            this.Y = b;
+
+        }
+
+      
+            
+
+
+      
+
+
+    }
+
+    class Rectangle : Figure
+    {
+        
+        public override void Draw()
+        {
+            Console.WriteLine("I`m Rectangle!");
+           
+        }
+
+        public Rectangle(double a, double b) : base (a, b)
+        {}
+
+    }
+
+    class Square : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("I`m Square!");
+        }
+
+        public Square(double a, double b) : base(a, b)
+        { }
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Enter X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+           
+            Figure obj1 = new Square(x, y);
+            Rectangle obj2 = new Rectangle(x, y);
+            obj1.Draw();          
+            obj2.Draw();
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
